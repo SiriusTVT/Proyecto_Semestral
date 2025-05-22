6a27ad1161db11cf63170c86c38225fd9dbfc6b6
Juan David Troncoso <juandavid.troncoso@gmail.com>
2025-05-21 21:11:40 -0500

    feat: remove dashboard route and services functionality

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,4 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard', {
-    dispositivos: [
-      { id: 'DR001', tipo: 'Drone', estado: 'Disponible', bateria: 87 },
-      { id: 'RB002', tipo: 'Robot', estado: 'En servicio', bateria: 45 }
-    ]
-  });
-});
-
 module.exports = router;
